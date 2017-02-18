9e61ee4d2e00afc869ae63cd041c8873fe679b7c
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-02-18 10:55:42 +0300

    Stylize breadcrumbs with resource name instead of full class name

--- templates/default/layout/html/setup.rb
+++ templates/default/layout/html/setup.rb
@@ -1,3 +1,7 @@
 def menu_lists
   super + [ { type: 'resource', title: 'Resources', search_title: 'Resource List' } ]
 end
+
+def resource_name(resource)
+  resource.name(true).gsub(/Controller$/, '')
+end
