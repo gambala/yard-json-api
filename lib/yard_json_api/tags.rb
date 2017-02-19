c9dd0b6f2cca419f3ac2399ca47222d4892a6307
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-02-19 16:48:44 +0300

    Reorder tag sections rendered through visible_tags array

--- lib/yard_json_api/tags.rb
+++ lib/yard_json_api/tags.rb
@@ -7,7 +7,9 @@ module YardJsonApi
 
     def self.define_tags
       TAGS.each { |tag| YARD::Tags::Library.define_tag(*tag) }
-      YARD::Tags::Library.visible_tags.place(:error).after(:example)
+      YARD::Tags::Library.visible_tags.delete(:param)
+      YARD::Tags::Library.visible_tags.place(:param).before(:example)
+      YARD::Tags::Library.visible_tags.place(:error).after(:param)
     end
   end
 end
