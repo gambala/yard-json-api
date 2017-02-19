182c407655fc41ced53daff44258a25e20ce7363
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-02-19 16:45:49 +0300

    Remove useless yard-restful tags definition

--- lib/yard_json_api/tags.rb
+++ lib/yard_json_api/tags.rb
@@ -1,25 +1,8 @@
 module YardJsonApi
   module Tags
     TAGS = [
-      ['Errors',                          :error, :with_types_and_title],
-      ['Endpoint',                        :endpoint],
-
-      ['URL',                             :url],
-      ['HTTP-Action for the Resource',    :action],
-
-      ['Resource Object Property',        :property, :with_types_and_name],
-
-      ['Required Arguments',              :required, :with_types_and_name],
-      ['Optional Arguments',              :optional, :with_types_and_name],
-
-      ['Example Request',                 :example_request],
-      ['Example Request Description',     :example_request_description],
-      ['Request Fields',                  :request_field, :with_types_and_name],
-
-      ['Example Response',                :example_response],
-      ['Example Response Description',    :example_response_description],
-      ['Response Fields',                 :response_field, :with_types_and_name],
-      ['Response Type',                   :response, :with_types]
+      ['Endpoint', :endpoint],
+      ['Errors',   :error, :with_types_and_title]
     ].freeze
 
     def self.define_tags
