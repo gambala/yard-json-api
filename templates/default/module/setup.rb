ed56be01d98f215bb534d16b02732f18c510dc7b
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-02-19 11:40:16 +0300

    Show only resource name in class page header
    With optional module/namespace suffix for Api/V1 like modules

--- templates/default/module/setup.rb
+++ templates/default/module/setup.rb
@@ -5,3 +5,12 @@ end
 def resource_name(resource)
   resource.name(true).gsub(/Controller$/, '')
 end
+
+def object_type(object)
+  case object
+  when YARD::CodeObjects::ClassObject
+    nil
+  else
+    object.type.to_s.capitalize
+  end
+end
