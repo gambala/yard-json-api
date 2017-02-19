acaf275dc0e7891dfc550d93b7160f452fe6f482
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-02-19 17:08:41 +0300

    Use @field instead of @param because of dirty validation warnings

--- lib/yard_json_api/tags.rb
+++ lib/yard_json_api/tags.rb
@@ -2,14 +2,14 @@ module YardJsonApi
   module Tags
     TAGS = [
       ['Endpoint', :endpoint],
+      ['Params',   :field, :with_types_and_name],
       ['Errors',   :error, :with_types_and_title]
     ].freeze
 
     def self.define_tags
       TAGS.each { |tag| YARD::Tags::Library.define_tag(*tag) }
-      YARD::Tags::Library.visible_tags.delete(:param)
-      YARD::Tags::Library.visible_tags.place(:param).before(:example)
-      YARD::Tags::Library.visible_tags.place(:error).after(:param)
+      YARD::Tags::Library.visible_tags.place(:field).before(:example)
+      YARD::Tags::Library.visible_tags.place(:error).after(:field)
     end
   end
 end
