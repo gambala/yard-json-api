b09ed93ae2c68ea761bc181d4dac7397c40dfdc6
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-02-20 09:43:51 +0300

    Replace @error tag by @response

--- lib/yard_json_api/tags.rb
+++ lib/yard_json_api/tags.rb
@@ -1,15 +1,15 @@
 module YardJsonApi
   module Tags
     TAGS = [
-      ['Endpoint', :endpoint],
-      ['Params',   :field, :with_types_and_name],
-      ['Errors',   :error, :with_types_and_title]
+      ['Endpoint',  :endpoint],
+      ['Params',    :field, :with_types_and_name],
+      ['Responses', :response, :with_types_and_title]
     ].freeze
 
     def self.define_tags
       TAGS.each { |tag| YARD::Tags::Library.define_tag(*tag) }
       YARD::Tags::Library.visible_tags.place(:field).before(:example)
-      YARD::Tags::Library.visible_tags.place(:error).after(:field)
+      YARD::Tags::Library.visible_tags.place(:response).after(:field)
     end
   end
 end
--- templates/default/tags/setup.rb
+++ templates/default/tags/setup.rb
@@ -1,6 +1,6 @@
 def init
   tags = Tags::Library.visible_tags - [:abstract, :deprecated, :note, :todo]
-  create_tag_methods(tags - [:example, :option, :overload, :see, :error])
-  sections :index, tags.map {|t| t.to_s.tr('.', '_').to_sym }
+  create_tag_methods(tags - [:example, :option, :overload, :see, :response])
+  sections :index, tags.map { |t| t.to_s.tr('.', '_').to_sym }
   sections.any(:overload).push(T('docstring'))
 end
