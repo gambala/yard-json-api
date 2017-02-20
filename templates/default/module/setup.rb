7eb8a00ec7d3742886a175a0748028ac5bd6c842
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-02-20 09:53:54 +0300

    Show only method details for @endpoint methods

--- templates/default/module/setup.rb
+++ templates/default/module/setup.rb
@@ -6,6 +6,10 @@ def resource_name(resource)
   resource.name(true).gsub(/Controller$/, '')
 end
 
+def meth_list
+  object.meths.select { |meth| meth.has_tag?(:endpoint) }
+end
+
 def object_type(object)
   case object
   when YARD::CodeObjects::ClassObject
