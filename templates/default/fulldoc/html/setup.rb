3d9aa6751acaeb902d6eba4867ec772d54bfdcb5
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-12 12:04:54 +0300

    Use reject instead of inverting select

--- templates/default/fulldoc/html/setup.rb
+++ templates/default/fulldoc/html/setup.rb
@@ -1,6 +1,6 @@
 def generate_class_list
   @items = Registry.all(:class)
-                   .select { |obj| !obj.meths.select { |meth| meth.has_tag?(:endpoint) }.empty? }
+                   .reject { |obj| obj.meths.select { |meth| meth.has_tag?(:endpoint) }.empty? }
                    .sort_by { |resource| resource_name(resource) }
   @list_title = 'Resource List'
   @list_type = 'class'
